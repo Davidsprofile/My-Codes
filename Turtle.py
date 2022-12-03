02ac1cf3162f84e44fa742978c96f49157f42af4
Davidsprofile <116757200+Davidsprofile@users.noreply.github.com>
2022-12-03 16:30:32 +0400

    Create Turtle.py

---
+++ Turtle.py
@@ -0,0 +1,37 @@
+# works only in environment
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+screen.listen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backwards():
+    tim.backward(10)
+
+
+def move_left():
+    tim.left(10)
+
+
+def move_right():
+    tim.right(10)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
